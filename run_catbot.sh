da46f1612cfbb71ecca76e976c752d7535b45a6b
Mei <mei.eisenbach@gmail.com>
2020-08-14 12:20:48 -0700

    Checks for DiscordKey in run_catbot.sh

--- run_catbot.sh
+++ run_catbot.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-. ./discord_key.sh
 
-while true
+DISCORD_KEY_FILE=./discord_key.sh
+
+PYTHON=python
+RESTART_DELAY_S=10
+
+if [[ ! -f "$DISCORD_KEY_FILE" ]];
+then
+   echo "Error: $DISCORD_KEY_FILE doesn't exist";
+   exit 1;
+else
+   . $DISCORD_KEY_FILE
+fi
+
+if [[ -z "$DiscordKey" ]];
+then
+   echo "Error: 'DiscordKey' variable must be exported from $DISCORD_KEY_FILE";
+   exit 2;
+fi
+
+while true;
 do
-	python catbot.py
-	sleep 10
+   echo "[Re-]starting catbot ..."
+   $PYTHON catbot.py
+   sleep $RESTART_DELAY_S
 done
