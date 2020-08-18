8520e9240fa406802ee177538973483d6c9141bc
Mei Eisenbach <mei.eisenbach@gmail.com>
2020-08-17 20:42:48 -0700

    Fixes code style warnings from autopep8

--- gyms.py
+++ gyms.py
@@ -7,38 +7,43 @@ Created on Tue Oct 16 10:15:51 2018
 import csv
 import string
 
-'''
 
-Exceptions:
-    raises IOError if the gym file cannot be opened 
-'''
+def process_name(name):
+    new_name = name.lower()
+
+    new_name = new_name.replace('’', "'")
+
+    table = str.maketrans({key: None for key in string.punctuation})
+    new_name = new_name.translate(table)
+
+    new_name = new_name.replace(' ', '')
+    return new_name
+
+
 class Gyms:
+    '''
+    A class for a list of Pokemon Go gyms
+
+    Methods:
+
+    Exceptions:
+        raises IOError if the gym file cannot be opened
+    '''
+
     gyms = {}
     aliases = {}
-    
+
     def __init__(self, gym_fname):
         self.read_csv(gym_fname)
-            
-        
-    def process_name(self, name):
-        new_name = name.lower()
-    
-        new_name = new_name.replace('’', "'")
-    
-        table = str.maketrans({key: None for key in string.punctuation})
-        new_name = new_name.translate(table) 
-    
-        new_name = new_name.replace(' ', '')
-        return new_name
-    
-    def read_csv(self, gym_fname):       
+
+    def read_csv(self, gym_fname):
         with open(gym_fname) as gymfile:
             gymreader = csv.DictReader(gymfile, delimiter=',', quotechar='"')
             for row in gymreader:
-                namekey = self.process_name(row['name']) 
+                namekey = process_name(row['name'])
                 self.gyms[namekey] = row
                 if len(row['alias']) > 0:
-                        self.aliases[self.process_name(row['alias'])] = namekey
+                    self.aliases[process_name(row['alias'])] = namekey
 
     def search_names(self, name):
         found = []
@@ -46,10 +51,10 @@ class Gyms:
             if name in namekey:
                 found.append(namekey)
         return found
-    
+
     def search_aliases(self, name):
         found = []
-    
+
         for alias in self.aliases:
             if name in alias:
                 found.append(self.aliases[alias])
@@ -57,21 +62,22 @@ class Gyms:
 
     def find(self, name):
         found = []
-       
-        new_name = self.process_name(name) 
+
+        new_name = process_name(name)
         if new_name in self.gyms.keys():
             found = [new_name]
         else:
             found = self.search_names(new_name)
             if len(found) == 0:
                 found = self.search_aliases(new_name)
-                
+
         return found
-        
+
     def get_link(self, gym):
         LINK_BASE = 'http://maps.google.com/maps?q='
-        return LINK_BASE + self.gyms[gym]['latitude'] + ',' + self.gyms[gym]['longitude']
-    
+        return LINK_BASE + self.gyms[gym]['latitude'] + ',' \
+            + self.gyms[gym]['longitude']
+
     def get_name(self, gym):
         return self.gyms[gym]['name']
 
