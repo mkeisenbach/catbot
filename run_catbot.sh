7fe602783bf29be488ae3eaadcec319e9afa85ed
Andre Eisenbach <andre@eisenbach.com>
2020-09-05 03:42:54 +0000

    Remove discord key handling from run_catbot.sh

--- run_catbot.sh
+++ run_catbot.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-DISCORD_KEY_FILE=./discord_key.sh
-
 PYTHON=python
 RESTART_DELAY_S=10
-
-if [[ ! -f "$DISCORD_KEY_FILE" ]];
-then
-   echo "Error: $DISCORD_KEY_FILE doesn't exist";
-   exit 1;
-else
-   . $DISCORD_KEY_FILE
-fi
-
-if [[ -z "$DiscordKey" ]];
-then
-   echo "Error: 'DiscordKey' variable must be exported from $DISCORD_KEY_FILE";
-   exit 2;
-fi
 
 while true;
 do
